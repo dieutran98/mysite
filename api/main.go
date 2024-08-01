c93afac5384c27882bf07c1206fa5933f82d7e2e
dieutran98 <dieu-tq@genkisystem.com>
2024-08-01 07:19:29 +0700

    add grateful shutdown

--- api/main.go
+++ api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log/slog"
 	"mysite/pkgs/database"
 	"mysite/pkgs/env"
@@ -8,6 +9,9 @@ import (
 	"mysite/router"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/pkg/errors"
 )
@@ -24,10 +28,37 @@ func main() {
 		}
 	}()
 
-	if err := http.ListenAndServe(":3000", router.InitRouter()); err != nil {
-		slog.Error("init router error", logger.AttrError(err))
+	srv := http.Server{
+		Addr:    ":3000",
+		Handler: router.InitRouter(),
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			slog.Error("failed to listen and serve", logger.AttrError(err))
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall. SIGKILL but can"t be catch, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	slog.Info("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		slog.Error("Server Shutdown:", logger.AttrError(err))
 		return
 	}
+	// catching ctx.Done(). timeout of 5 seconds.
+	select {
+	case <-ctx.Done():
+		slog.Info("timeout of 5 seconds.")
+	}
+	slog.Info("Server exiting")
 }
 
 func setup() error {
